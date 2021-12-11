1664dde473731c6eed13d3456ba198c0e545d516
sabri <sabri_51@hotmail.com>
2021-12-11 17:14:59 +0300

    Docker file updated

--- code/app/src/components/latest/index.tsx
+++ code/app/src/components/latest/index.tsx
@@ -1,24 +1,24 @@
-import {Box, Divider} from "@material-ui/core";
+import { Box, Divider } from "@material-ui/core";
 import React from "react";
 import MediaCard from "../card";
 import Typography from "@mui/material/Typography";
 
-
 export default function Latest() {
-    return (
-<React.Fragment>
- <Box sx={{display:"flex",flexDirection:"column",marginTop:"100px"}}>
-     <Typography component="h3" color="black" variant="h6">
-         Latest
-     </Typography>
-     <Divider variant="middle" style={{margin:0}}/>
-     <Box sx={{display:"flex",marginTop:"10px"}}>
-         <MediaCard/>
-         <MediaCard/>
-         <MediaCard />
-     </Box>
- </Box>
-
-</React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <Box
+        sx={{ display: "flex", flexDirection: "column", marginTop: "100px" }}
+      >
+        <Typography component="h3" color="black" variant="h6">
+          Latest Updated
+        </Typography>
+        <Divider variant="middle" style={{ margin: 0 }} />
+        <Box sx={{ display: "flex", marginTop: "10px" }}>
+          <MediaCard />
+          <MediaCard />
+          <MediaCard />
+        </Box>
+      </Box>
+    </React.Fragment>
+  );
 }
