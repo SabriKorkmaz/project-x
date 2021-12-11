080531419391b020de9e72dde0c1bf94049f0103
sabri <sabri_51@hotmail.com>
2021-12-11 15:15:11 +0300

    service and meetup request completed

--- code/app/src/components/footer/index.tsx
+++ code/app/src/components/footer/index.tsx
@@ -1,32 +1,53 @@
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import {Box, Chip} from "@material-ui/core";
-import {Stack, Toolbar} from '@mui/material';
-
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { Box, Chip } from "@material-ui/core";
+import { Stack, Toolbar } from "@mui/material";
 
 export default function Footer() {
-    const handleClick = () => {
-        console.info('You clicked the Chip.');
-    };
-    return (
-        <Box color="primary" style={{bottom: 0, position: "fixed",width:"500px"}}>
-            <Container maxWidth="lg" style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-                <Box>
-                    <Toolbar>
-                        <Typography variant="body1" color="inherit">
-                            © SWE 573
-                        </Typography>
-                    </Toolbar>
-                </Box>
-                <Box>
-                    <Stack direction="row" spacing={2}>
-                        <Chip size="medium" label="How it works?" variant="outlined" onClick={handleClick} />
-                        <Chip size="medium" label="GDPR" variant="outlined" onClick={handleClick} />
-                        <Chip size="medium" label="Cookie Policy" variant="outlined" onClick={handleClick} />
-                    </Stack>
-
-                </Box>
-            </Container>
+  const handleClick = () => {};
+  return (
+    <Box
+      color="primary"
+      style={{ bottom: 0, position: "fixed", width: "500px" }}
+    >
+      <Container
+        maxWidth="lg"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box>
+          <Toolbar>
+            <Typography variant="body1" color="inherit">
+              © SWE 573
+            </Typography>
+          </Toolbar>
         </Box>
-    );
+        <Box>
+          <Stack direction="row" spacing={2}>
+            <Chip
+              size="medium"
+              label="How it works?"
+              variant="outlined"
+              onClick={handleClick}
+            />
+            <Chip
+              size="medium"
+              label="GDPR"
+              variant="outlined"
+              onClick={handleClick}
+            />
+            <Chip
+              size="medium"
+              label="Cookie Policy"
+              variant="outlined"
+              onClick={handleClick}
+            />
+          </Stack>
+        </Box>
+      </Container>
+    </Box>
+  );
 }
